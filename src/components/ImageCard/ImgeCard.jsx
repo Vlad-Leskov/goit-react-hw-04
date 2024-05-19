459357874c7823d058d138393a2cf750dfa17346
Vlad-Leskov <tyabaka@gmail.com>
2024-05-19 11:56:09 +0300

    3

--- src/components/ImageCard/ImgeCard.jsx
+++ src/components/ImageCard/ImgeCard.jsx
@@ -1,11 +1,12 @@
 import css from "./ImgeCard.module.css";
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div className={css.card} onClick={() => onClick(image)}>
+    <div className={css.card}>
       <img
         className={css.image}
         src={image.urls.small}
         alt={image.alt_description}
+        onClick={() => onClick(image)}
       />
     </div>
   );
